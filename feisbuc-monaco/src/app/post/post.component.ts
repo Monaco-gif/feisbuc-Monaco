aff79735c8d0043c1c86f40799ae710de4745a1c
Monaco-gif <monaco.matteo@iisgalvanimi.edu.it>
2021-05-02 23:17:13 +0000

    ho finitooooo

--- feisbuc-monaco/src/app/post/post.component.ts
+++ feisbuc-monaco/src/app/post/post.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
+  @Input() post: Post;
+  commento = new Array<string>();
+
+  Like(): boolean{
+    this.post.voteUp();
+    return false;
+  }
+
+  addCommento(commento: HTMLInputElement): boolean{
+    this.commento.push(commento.value);
+    return false;
+  }
 
   constructor() { }
 
